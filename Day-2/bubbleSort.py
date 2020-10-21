354879d5e2b2756dc815b1b853fcfac81461a3dc
Chanchal K Maurya <chanchalkumarmaurya2299@gmail.com>
2020-10-21 10:18:52 +0530

    added a bubbleSort.py file

---
+++ Day-2/bubbleSort.py
@@ -0,0 +1,15 @@
+#Python Program to implement Bubble Sort
+
+def bubbleSort(arr):
+    for _ in range(len(arr)):
+        for index in range(len(arr)-1):
+            if arr[index] > arr[index+1]:
+                arr[index],arr[index+1] = arr[index+1],arr[index]
+    return arr
+
+if __name__ == "__main__":
+    arr = [5,99,1,5,7,9]
+    res = bubbleSort(arr)
+    for i in res:
+        print(i,end = " ")
+    print()
